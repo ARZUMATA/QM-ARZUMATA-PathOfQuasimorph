f72ad01527efbce775717144ba62930eafa01d7e
ARZUMATA <arzumata@gmail.com>
2025-06-28 17:58:48 +0300

    more patches

--- src/Core/PathOfQuasimorph_OK.cs
+++ src/Core/PathOfQuasimorph_OK.cs
@@ -80,5 +80,68 @@ namespace QM_PathOfQuasimorph.Core
                 }
             }
         }
+
+        // Access to MagnumProjects instance class.
+        [HarmonyPatch(typeof(MagnumDevelopmentSystem), nameof(MagnumDevelopmentSystem.Update))]
+        public static class MagnumDevelopmentSystem_Update_Patch
+        {
+            public static void Postfix(MagnumProjects magnumProjects, SpaceTime time)
+            {
+                if (magnumProjectsController == null)
+                {
+                    Plugin.Logger.LogWarning("MagnumPoQProjectsController missing. Creating one.");
+                    magnumProjectsController = new MagnumPoQProjectsController(magnumProjects);
+                    Plugin.Logger.Log($"\t\t test: {magnumProjectsController.magnumProjects.Values.Count}"); // What projects we made in-game.
+                }
+            }
+        }
+
+        // We reuse original method with edits.
+        // We add check for our custom projects via DateTime.StartTime.
+        // Normal projects don't have DateTime.MinValue and it's good for finding them later on.
+        [HarmonyPatch(typeof(MagnumProjects), nameof(MagnumProjects.Get))]
+        public static class MagnumProjects_Get_Patch
+        {
+            // Default method copied here.
+            [HarmonyPrefix]
+            public static bool Get(string devId, ref MagnumProject __result, MagnumProjects __instance)
+            {
+                foreach (MagnumProject magnumProject in __instance.Values)
+                {
+                    if (magnumProject.DevelopId.Equals(devId))
+                    {
+                        __result = magnumProject;
+                        return false; // Return false to skip the original method
+                    }
+
+                    // PathOfQuasimorph ADD START
+                    // DateTime MinValue = new DateTime(0L, DateTimeKind.Unspecified);
+                    // DateTime MaxValue = new DateTime(3155378975999999999L, DateTimeKind.Unspecified);
+                    // Both Int64
+
+                    if (magnumProject.StartTime == DateTime.MinValue)
+                    {
+                        __result = null;
+                        return false; // Return false to skip the original method
+                    }
+
+                    // PathOfQuasimorph END START
+                }
+
+                __result = null;
+                return false; // Return false to skip the original method
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
