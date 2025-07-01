0e67021f76625af9731bf772eada73949c2a9c57
ARZUMATA <arzumata@gmail.com>
2025-07-01 21:23:49 +0300

    HexStringToUnityColor

--- src/Helpers.cs
+++ src/Helpers.cs
@@ -11,6 +11,33 @@ internal static class Helpers
     public static string AlphaAwareColorToHex(Color color) =>
         $"#{(int)(color.r * 255):X2}{(int)(color.g * 255):X2}{(int)(color.b * 255):X2}{(int)(color.a * 255):X2}";
 
+    public static Color HexStringToUnityColor(string hex)
+    {
+        if (string.IsNullOrEmpty(hex) || !hex.StartsWith("#"))
+        {
+            throw new ArgumentException("Invalid color format", nameof(hex));
+        }
+
+        // Check the length of the input string to determine if it has an alpha channel
+        if (hex.Length == 7)  // No alpha channel provided, assume full opacity
+        {
+            hex = "#" + hex.Substring(1) + "FF";
+        }
+        else if (hex.Length != 9)
+        {
+            throw new ArgumentException("Invalid color format", nameof(hex));
+        }
+
+        // Parse the R, G, B, and A values from the hex string
+        int r = int.Parse(hex.Substring(1, 2), System.Globalization.NumberStyles.HexNumber);
+        int g = int.Parse(hex.Substring(3, 2), System.Globalization.NumberStyles.HexNumber);
+        int b = int.Parse(hex.Substring(5, 2), System.Globalization.NumberStyles.HexNumber);
+        int a = int.Parse(hex.Substring(7, 2), System.Globalization.NumberStyles.HexNumber);
+
+        // Convert to normalized float values
+        return new Color(r / 255f, g / 255f, b / 255f, a / 255f);
+    }
+
     public static string GetMd5HashFromFilePath(string filePath)
     {
         using (var md5 = MD5.Create())
