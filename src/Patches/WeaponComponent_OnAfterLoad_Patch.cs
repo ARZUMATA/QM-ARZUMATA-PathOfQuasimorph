72776c116416d86ed99925e24dcad349aa8ade8e
ARZUMATA <arzumata@gmail.com>
2025-07-04 02:42:54 +0300

    better log for todo fix

--- src/Patches/WeaponComponent_OnAfterLoad_Patch.cs
+++ src/Patches/WeaponComponent_OnAfterLoad_Patch.cs
@@ -27,8 +27,8 @@ namespace QM_PathOfQuasimorph.Core
                 }
                 catch
                 {
-                    Plugin.Logger.LogWarning($"WeaponComponent: Error on get GetSimpleRecord");
-                    Plugin.Logger.LogWarning($"WeaponComponent: reverting id to the baseline.");
+                    Plugin.Logger.LogWarning($"WARNING: WeaponComponent: Error on get GetSimpleRecord");
+                    Plugin.Logger.LogWarning($"WARNING: WeaponComponent: reverting id to the baseline.");
 
                     var magnumProjectWrapper = MagnumProjectWrapper.SplitItemUid(__instance._weaponId);
                     __instance._weaponId = magnumProjectWrapper.Id; // This effectively makes item default.
