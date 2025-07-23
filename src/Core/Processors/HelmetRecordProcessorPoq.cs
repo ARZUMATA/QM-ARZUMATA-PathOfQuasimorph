c3b55a3ce2af73c1e2dc3d721c95a4456997ac2a
ARZUMATA <arzumata@gmail.com>
2025-07-23 08:26:17 +0300

    item and armor processors

---
+++ src/Core/Processors/HelmetRecordProcessorPoq.cs
@@ -0,0 +1,123 @@
+﻿using MGSC;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+using static MGSC.SpawnSystem;
+using static MGSC.TurnDebugLogger;
+using Random = System.Random;
+
+namespace QM_PathOfQuasimorph.Core.Processors
+{
+    internal class HelmetRecordProcessorPoq : ItemRecordProcessor<HelmetRecord>
+    {
+        //ArmorRecord armorRecord;
+        private new Logger _logger = new Logger(null, typeof(HelmetRecordProcessorPoq));
+
+        public override List<string> parameters => _parameters;
+
+        internal List<string> _parameters = new List<string>()
+        {
+            "resist_blunt",
+            "resist_pierce",
+            "resist_lacer",
+            "resist_fire",
+            "resist_beam",
+            "resist_shock",
+            "resist_poison",
+            "resist_cold",
+            "weight",
+            "max_durability",
+            //"none"
+        };
+
+        public HelmetRecordProcessorPoq(ItemRecordsControllerPoq itemRecordsControllerPoq) : base(itemRecordsControllerPoq)
+        {
+        }
+
+        internal override int ProcessRecord()
+        {
+            if (itemRarity == ItemRarity.Standard)
+            {
+                return 99;
+            }
+
+            int boostedStat;
+
+            boostedStat = ApplyParameters();
+
+            return boostedStat;
+        }
+
+        internal int ApplyParameters()
+        {
+            float baseModifier, finalModifier;
+            int numToHinder, numToImprove, boostedParam, improvedCount, hinderedCount;
+            string boostedParamString;
+            bool increase;
+            PrepGenericData(out baseModifier, out finalModifier, out numToHinder, out numToImprove, out boostedParam, out boostedParamString, out improvedCount, out hinderedCount, out increase);
+
+            // Get average resist for armor.
+            float averageResist = 0;
+            int resistCount = 0;
+
+            var resistSheet = itemRecord.ResistSheet;
+
+            foreach (var res in resistSheet)
+            {
+                //_logger.Log($"\t\t Resist: {param.Id}");
+
+                // If there is only one resist, it leads to imbalance as it can get applied to others.
+                averageResist += res.resistPercent;
+                resistCount++;
+            }
+
+            averageResist = (float)Math.Round(averageResist / resistCount, 2);
+            averageResist = Math.Max(averageResist, 1.0f); // Ensure average resist is at least 1.0
+            _logger.Log($"\t\t\t\t Average resist {averageResist} for total count {resistCount}");
+
+            foreach (var stat in parameters)
+            {
+                finalModifier = GetFinalModifier(baseModifier, numToHinder, numToImprove, ref improvedCount, ref hinderedCount, boostedParamString, ref increase, stat, _logger);
+
+                // Simply for logging
+                float outOldValue = -1;
+                float outNewValue = -1;
+
+                if (stat.Contains("resist"))
+                {
+                    var resistName = stat.Split('_')[1]; // :D
+                    var resistValue = itemRecord.GetResist(resistName);
+                    PathOfQuasimorph.raritySystem.ApplyModifier<float>(ref resistValue, finalModifier, increase, out outOldValue, out outNewValue);
+                    itemRecord.SetResist(resistName, outNewValue);
+                }
+                else
+                {
+                    switch (stat)
+                    {
+                        case "weight":
+                            PathOfQuasimorph.raritySystem.Apply<float>(v => itemRecord.Weight = v, () => itemRecord.Weight, finalModifier, increase, out outOldValue, out outNewValue);
+                            break;
+
+                        case "max_durability":
+                            PathOfQuasimorph.raritySystem.Apply<int>(v => itemRecord.MaxDurability = v, () => itemRecord.MaxDurability, finalModifier, increase, out outOldValue, out outNewValue);
+                            break;
+
+                        case "none":
+                            break;
+                    }
+                }
+
+                Plugin.Logger.Log($"\t\t old value {outOldValue}");
+                Plugin.Logger.Log($"\t\t new value {outNewValue}");
+            }
+
+            return boostedParam;
+        }
+    }
+}
---
+++ src/Core/Processors/ItemRecordProcessor.cs
@@ -0,0 +1,124 @@
+﻿using MGSC;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using static MGSC.SpawnSystem;
+using static MGSC.TurnDebugLogger;
+using Random = System.Random;
+
+namespace QM_PathOfQuasimorph.Core.Processors
+{
+    internal abstract class ItemRecordProcessor<T>
+    {
+        protected T itemRecord;
+        protected ItemRecordsControllerPoq itemRecordsControllerPoq;
+        protected Logger _logger = new Logger(null, typeof(ItemRecordProcessor<T>));
+
+        public abstract List<string> parameters { get; }
+        protected ItemRarity itemRarity;
+        protected bool mobRarityBoost;
+
+        internal ItemRecordProcessor(ItemRecordsControllerPoq itemRecordsControllerPoq)
+        {
+            this.itemRecordsControllerPoq = itemRecordsControllerPoq;
+        }
+
+        internal virtual void Init(T itemRecord, ItemRarity itemRarity, bool mobRarityBoost)
+        {
+            this.itemRecord = itemRecord;
+            this.itemRarity = itemRarity;
+            this.mobRarityBoost = mobRarityBoost;
+        }
+
+        internal abstract int ProcessRecord();
+
+        internal float GetFinalModifier(float baseModifier, int numToHinder, int numToImprove, ref int improvedCount, ref int hinderedCount, string boostedParamString, ref bool increase, string stat, Logger _logger)
+        {
+            float finalModifier;
+
+            if (new[] { "_weight", "_reload_duration", "_scatter_angle" }.Any(stat.Equals))
+            {
+                increase = false;
+            }
+            else if (new[] { "_resist", "_damage", "_crit_damage", "_max_durability", "_accuracy", "_magazine_capacity" }
+                     .Any(stat.Equals))
+            {
+                increase = true;
+            }
+
+            _logger.Log($"Updating {stat}");
+
+            // Apply boost
+            if (stat == boostedParamString)
+            {
+                finalModifier = baseModifier * (float)Math.Round(Helpers._random.NextDouble() * (RaritySystem.PARAMETER_BOOST_MAX - RaritySystem.PARAMETER_BOOST_MIN) + RaritySystem.PARAMETER_BOOST_MIN, 2);
+
+                _logger.Log($"\t\t boosting final modifier from {baseModifier} to {finalModifier}");
+            }
+            else
+            {
+                finalModifier = baseModifier;
+            }
+
+            // Determine if we should hinder this parameter
+            bool hinder = PathOfQuasimorph.raritySystem.ShouldHinderParameter(ref hinderedCount, ref improvedCount, numToHinder, numToImprove);
+
+            if (hinder)
+            {
+                increase = !increase;
+            }
+
+            _logger.Log($"\t\t finalModifier: {finalModifier} hinder: {hinder}, boosted: {finalModifier != baseModifier}");
+            return finalModifier;
+        }
+
+        internal void PrepGenericData(out float baseModifier, out float finalModifier, out int numToHinder, out int numToImprove, out int boostedParam, out string boostedParamString, out int improvedCount, out int hinderedCount, out bool increase)
+        {
+            baseModifier = PathOfQuasimorph.raritySystem.GetRarityModifier(itemRarity, PathOfQuasimorph.raritySystem._rarityModifiers);
+            if (mobRarityBoost)
+            {
+                baseModifier *= baseModifier;
+            }
+
+            finalModifier = 0;
+            var (Min, Max) = PathOfQuasimorph.raritySystem.rarityParamPercentages[itemRarity];
+
+            int minParams = Math.Max(0, (int)Math.Floor(Min * parameters.Count));
+            int maxParams = (int)Math.Ceiling(Max * parameters.Count);
+
+            // Calculate the number of parameters to adjust based on the percentage
+            int numToAdjust = Helpers._random.Next(minParams, maxParams + 1);
+
+            numToHinder = (int)Math.Floor(numToAdjust * PathOfQuasimorph.raritySystem.PARAMETER_HINDER_PERCENT / 100f);
+            numToImprove = numToAdjust - numToHinder;
+
+            // Shuffle the list
+            Helpers.ShuffleList(parameters);
+
+            // Select one parameter to boost more.
+            // This parameter will be boosted more than the others.
+            // We return index of parameter that was boosted for UID
+            boostedParam = Helpers._random.Next(parameters.Count);
+            boostedParamString = parameters[boostedParam];
+
+            // Counters to track how many parameters we've improved or hindered
+            improvedCount = 0;
+            hinderedCount = 0;
+
+            // Determine if we need increase or decrease
+            increase = true;
+        }
+
+        //protected abstract List<DmgResist> GetResistSheet();
+        //protected abstract float GetResist(string resistName);
+        //protected abstract void SetResist(string resistName, float value);
+        //protected abstract float GetWeight();
+        //protected abstract void SetWeight(float value);
+        //protected abstract int GetMaxDurability();
+        //protected abstract void SetMaxDurability(int value);
+    }
+}
